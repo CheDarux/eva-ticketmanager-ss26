b88728c8b5e8750fb2850c454b8cb548ff88cbbd
CheDarux <che.darux@gmail.com>
2026-04-23 14:42:56 +0200

    implement CustomerService.createCustomer()

--- src/main/java/Core/Services/CustomerService.java
+++ src/main/java/Core/Services/CustomerService.java
@@ -16,9 +16,24 @@ public class CustomerService implements CustomerServiceInterface {
         LocalDate dateOfBirth
     ) throws IllegalArgumentException {
 
-        //TODO
+        boolean multipleAtSymbols = email.chars().filter(ch -> ch == '@').count() > 1;
 
-        return null;
+        if (!email.contains("@") || multipleAtSymbols) {
+            throw new CustomerException("Invalid email");
+        }
+
+        if(dateOfBirth.isAfter(LocalDate.now().minusYears(18))) {
+            throw new CustomerException("User has to be 18 years old");
+        }
+
+        Customer customer = new Customer(UUID.randomUUID(), username, email, dateOfBirth);
+
+        return new Customer(
+            customer.getId(),
+            customer.getUsername(),
+            customer.getEmail(),
+            customer.getDateOfBirth()
+        );
     }
 
     public Customer getCustomerById(UUID id) throws CustomerException {
